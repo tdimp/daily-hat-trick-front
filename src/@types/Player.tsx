2096a2092a373e36c90643a3ec3a2dad3497d186
Thomas Dimperio <twdimperio@gmail.com>
2022-12-27 10:42:20 -0700

    Init Player interface and Player component placeholder

---
+++ src/@types/Player.tsx
@@ -0,0 +1,6 @@
+export interface Player {
+  name: string;
+  position: string;
+  jerseyNumber: number;
+  nhlTeam: string;
+}
---
+++ src/components/Player.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Player } from '../@types/Player';
+
+const Player = () => {
+  return (
+    <div>
+      <h2>Player Name</h2>
+      <h3>Position</h3>
+      <h3>Jersey Number</h3>
+      <h3>NHL Team</h3>
+      <h3>Stats...</h3>
+    </div>
+  )
+}
+
+export default Player
