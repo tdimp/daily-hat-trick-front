4ac8538299ae488592ce205100c6804b33dbf399
Thomas Dimperio <twdimperio@gmail.com>
2023-01-07 16:50:56 -0700

    Add hasAccount toggle functionality to Login and Signup

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,9 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react';
+import { UserContext } from '../context/UserContext';
+import SignUp from './SignUp';
 
 const Login = () => {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [hasAccount, setHasAccount] = useState(true);
+
+  const {user, setUser} = useContext(UserContext);
+
+  const handleHasAccountClick = () => {
+    setHasAccount(!hasAccount);
+  }
 
   const handleUserLogin = async (e: React.SyntheticEvent) => {
     e.preventDefault();
@@ -21,6 +30,7 @@ const Login = () => {
 
     const data = await response.json();
     if (response.ok) {
+      setUser(data);
       alert("Logged in!")
     } else {
       alert(data.error.login)
@@ -29,17 +39,25 @@ const Login = () => {
     setPassword("");
   }
 
-  return (
-    <div>
-      <form onSubmit={handleUserLogin}>
-        <label>Email*</label>
-        <input type="text" required={true} value={email} onChange={(e) => setEmail(e.target.value)} /><br />
-        <label>Password*</label>
-        <input type="password" required={true} value={password} onChange={(e) => setPassword(e.target.value)} /><br />
-        <input type="submit" value="Login!" />
-      </form>
-    </div>
-  )
+  if(!user && hasAccount) {
+    return (
+      <div>
+        <form onSubmit={handleUserLogin}>
+          <label>Email*</label>
+          <input type="text" required={true} value={email} onChange={(e) => setEmail(e.target.value)} />
+          <label>Password*</label>
+          <input type="password" required={true} value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input type="submit" value="Login!" />
+        </form>
+        <>Don't have an account?</>
+        <button onClick={handleHasAccountClick}>Sign Up</button>
+      </div>
+    )
+  } else if(!user && !hasAccount) {
+    return <SignUp />
+  } else {
+    return null;
+  }
 }
 
 export default Login
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../context/UserContext';
+import Login from './Login';
 
 const SignUp = () => {
 
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [hasAccount, setHasAccount] = useState(false);
 
-  //const navigate = useNavigate();
+  const {user, setUser} = useContext(UserContext);
+
+  const navigate = useNavigate();
 
   interface User { // TypeScript interface defining the data types of users
     email: string;
@@ -15,7 +20,9 @@ const SignUp = () => {
     password: string;
   }
 
-  // Need a reroute to '/' upon successful creation and log in of new user
+  const handleHasAccountClick = () => {
+    setHasAccount(!hasAccount);
+  }
 
   const handleUserSignUp = async (e: React.SyntheticEvent) => {
     e.preventDefault()
@@ -35,32 +42,38 @@ const SignUp = () => {
 
     const data = await response.json();
     if (response.ok) {
-      // TODO: Update Redux store with created user data
-      alert(`User ${user.username} created!`)
+      setUser(data);
+      alert(`User ${user.username} created!`);
     } else {
-      alert("Oops, something went wrong.")
+      alert("Oops, something went wrong.");
     }
     setEmail("");
     setUsername("");
     setPassword("");
-    //navigate('/')
+    navigate('/')
   }
 
-
-  return ( // Controlled sign up form goes here
-           // Fields: Email address, Username, Password, Password Confirmation
-    <div>
-      <form onSubmit={handleUserSignUp}>
-        <label>Email*</label>
-          <input type="text" required={true} value={email} onChange={(e) => setEmail(e.target.value)} /><br />
-        <label>Username*</label>
-          <input type="text" required={true} value={username} onChange={(e) => setUsername(e.target.value)} /><br />
-        <label>Password*</label>
-          <input type="password" required={true} value={password} onChange={(e) => setPassword(e.target.value)} />
-        <input type="submit" value="Sign up!" />
-      </form>
-    </div>
-  )
+  if(!user && !hasAccount) {
+    return (
+      <div>
+        <form onSubmit={handleUserSignUp}>
+          <label>Email*</label>
+            <input type="text" required={true} value={email} onChange={(e) => setEmail(e.target.value)} /><br />
+          <label>Username*</label>
+            <input type="text" required={true} value={username} onChange={(e) => setUsername(e.target.value)} /><br />
+          <label>Password*</label>
+            <input type="password" required={true} value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input type="submit" value="Sign up!" />
+        </form>
+        <>Already have an account?</>
+        <button onClick={handleHasAccountClick}>Log In</button>
+      </div>
+    )
+  } else if(!user && hasAccount) {
+    return <Login />
+  } else {
+    return null;
+  }
 }
 
 export default SignUp
