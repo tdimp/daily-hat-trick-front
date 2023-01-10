447dc5e85e4820a6d5afdf9755b17b8e1ac5bf59
Thomas Dimperio <twdimperio@gmail.com>
2023-01-09 17:49:08 -0700

    Build team fetch, populate table with player data for given team

--- src/Containers/Team.tsx
+++ src/Containers/Team.tsx
@@ -1,18 +1,88 @@
-import React from 'react'
-import { TeamInterface } from '../@types/Team'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Player from '../components/Player';
+import { PlayerInterface } from '../@types/Player';
+import { SkaterStatsInterface, GoalieStatsInterface } from '../@types/Stats';
 
 
 
-const Team = ({ name }: TeamInterface) => {
+const Team = () => {
+
+  const [team, setTeam] = useState([]);
+  const { id } = useParams();
+
+  const tableRows = ['Name', 'G', 'A', 'PIM', 'PPP', 'W', 'GAA', 'SV%', 'SO', 'TOI'];
 
   // Fetch to teams#show on backend...
-  console.log()
+  useEffect(() => {
+    fetch(`/teams/${id}`)
+      .then(res => res.json())
+      .then(data => setTeam(data))
+      .catch(error => alert(error))
+  }, []);
   
+  const handleSortClick = () => {
+    console.log("Clicked!") //https://blog.logrocket.com/creating-react-sortable-table/#creating-the-table-markup-in-react
+  }
+
   return (
-    <div>
-      {name} Hellod
+    <div className='table'>
+      <table>
+        <thead>
+          <tr>
+            {tableRows.map((row) => <th onClick={handleSortClick}>{row}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          {team.map((player: PlayerInterface) => (
+            <tr key={player.id}>
+              <td>{`${player.full_name}, ${player.position}`}</td>
+              { player.position !== 'G' ? 
+                <>
+                  <td>{player.skater_stats[0].goals}</td>
+                  <td>{player.skater_stats[0].assists}</td>
+                  <td>{player.skater_stats[0].pim}</td>
+                  <td>{player.skater_stats[0].power_play_points}</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>{player.skater_stats[0].time_on_ice}</td>
+                </> 
+                : 
+                <>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>{player.goalie_stats[0].wins}</td>
+                  <td>{player.goalie_stats[0].goals_against_average}</td>
+                  <td>{player.goalie_stats[0].save_percentage}</td>
+                  <td>{player.goalie_stats[0].shutouts}</td>
+                  <td>{player.goalie_stats[0].time_on_ice}</td>
+                </>
+                }
+            </tr>
+          ))}
+        </tbody>
+        <tbody>
+
+        </tbody>
+      </table>
     </div>
   )
+
+  /*
+  return (
+    <div>
+      {team?.map((player: PlayerInterface) => {
+        return (
+          <Player key={player.id} id={player.id} full_name={player.full_name} position={player.position} jersey_number={player.jersey_number} nhl_team={player.nhl_team} skater_stats={player.skater_stats} goalie_stats={player.goalie_stats} />
+        )
+      })} 
+    </div>
+  )*/
 }
+
 
 export default Team
