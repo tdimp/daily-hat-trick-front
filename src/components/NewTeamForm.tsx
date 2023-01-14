2a13024dfaec9d34faf443ae7b03865898e62678
Thomas Dimperio <twdimperio@gmail.com>
2023-01-14 16:31:13 -0700

    Refactor NewTeamForm to update state within TeamList component

--- src/components/NewTeamForm.tsx
+++ src/components/NewTeamForm.tsx
@@ -1,13 +1,21 @@
 import React, { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../context/UserContext';
 import { TeamInterface } from '../@types/TeamInterface';
+import { Navigate } from 'react-router-dom';
 
 // Needs User from UserContext for user-team association
 
-const NewTeamForm = () => {
+interface Props {
+  handleCreate: (arg: TeamInterface) => void | undefined;
+}
+
+const NewTeamForm = ({ handleCreate}: Props) => {
   const userContext = useContext(UserContext);
-  const [teamName, setTeamName] = useState("");
-  
+  const [teamName, setTeamName] = useState('');
+
+  const navigate = useNavigate();
+
   // POST to teams#create
   const onSubmit = async (e: React.SyntheticEvent) => {
     e.preventDefault();
@@ -16,18 +24,19 @@ const NewTeamForm = () => {
       user_id: userContext.user?.userId,
     }
 
-    const response = await fetch("/teams", {
-      method: "POST",
+    const response = await fetch('/teams', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(teamData),
     });
 
     const data = await response.json();
     if (response.ok) {
-      alert("Team created!")
-      console.log(data);
+      handleCreate(data);
+      alert('Team created!');
+      //navigate('/teams');
     } else {
       console.log(data);
       alert(data.error);
@@ -42,10 +51,10 @@ const NewTeamForm = () => {
   return (
     <div>
       <form onSubmit = {onSubmit}>
-        <label>Team Name <br />
-          <input type="text" required={true} value={teamName} onChange={(e) => setTeamName(e.target.value)} />
+        <label>Create New Team<br />
+          <input type='text' required={true} value={teamName} onChange={(e) => setTeamName(e.target.value)} />
         </label>
-        <input type="submit" value="Create Team" />
+        <input type='submit' value='Create Team' />
       </form>
     </div>
   )
