03da2ea869eed9844c8feb06571b0df5b387f9ef
Thomas Dimperio <twdimperio@gmail.com>
2023-01-14 16:38:00 -0700

    Initialize TeamContext component

---
+++ src/context/TeamContext.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// Need to create TeamContext to provide team list to PlayerList component.
+// This will enable users to select which team to add players to from the PlayerList.
+
+export const TeamContext = () => {
+  return (
+    <div>TeamContext</div>
+  )
+}
