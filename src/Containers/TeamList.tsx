94711cd61f778a91e5171fa063b501711c494826
Thomas Dimperio <twdimperio@gmail.com>
2023-01-14 16:32:40 -0700

    Move NewTeamForm to TeamList. Update state upon team creation

--- src/Containers/TeamList.tsx
+++ src/Containers/TeamList.tsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../context/UserContext';
 import { TeamInterface } from '../@types/TeamInterface';
+import NewTeamForm from '../components/NewTeamForm';
 
-const TeamList = ( ) => {
+const TeamList = () => {
   const [teams, setTeams] = useState<TeamInterface[] | []>([]);
 
   const {user, setUser} = useContext(UserContext);
@@ -18,12 +19,17 @@ const TeamList = ( ) => {
     }
   }, [user])
 
+  const handleCreateTeam = (newTeam: TeamInterface) => {
+    setTeams([...teams, newTeam])
+  }
+
   if(!user) {
     return <h3 className='need-auth'>You must be logged in to view this page.</h3>
   }
 
   return (
     <>
+      <NewTeamForm handleCreate={handleCreateTeam}/>
       {teams.map((team) => 
         <Link to ={`/teams/${team.id}`} className="link" key={team.id}>{team.name}</Link>
       )}
