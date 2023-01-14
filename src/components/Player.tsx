c7c86ab05ec34fce46c823a829776c220231bbdc
Thomas Dimperio <twdimperio@gmail.com>
2023-01-13 20:31:40 -0700

    Add console.log to check attributes

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { PlayerInterface } from '../@types/Player';
+import { PlayerInterface } from '../@types/PlayerInterface';
 import { stat } from 'fs';
-import { SkaterStatsInterface } from '../@types/Stats';
+import { SkaterStatsInterface } from '../@types/StatsInterface';
+import PlayerList from '../Containers/PlayerList';
 
 
 
 const Player = ({ full_name, nhl_team, jersey_number, position, skater_stats, goalie_stats }: PlayerInterface) => {
-  
-console.log(skater_stats.length ? skater_stats[0].goals : goalie_stats[0])
+  console.log(goalie_stats.length ? goalie_stats : 'skater')
 
   return (
     <div>
       <h2>{full_name}, {nhl_team.name}, {position}</h2>
+     
     </div>
   )
 }
