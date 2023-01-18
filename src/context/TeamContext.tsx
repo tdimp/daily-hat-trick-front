644d990c086a33d15bbfd479e0b07f2640bfd24b
Thomas Dimperio <twdimperio@gmail.com>
2023-01-17 20:08:13 -0700

    Initialize TeamContext and TeamsProvider to pass teams data to different components

--- src/context/TeamContext.tsx
+++ src/context/TeamContext.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { createContext, useState, useEffect } from 'react';
+import { TeamInterface, TeamContextType } from '../@types/TeamInterface';
+import { Props } from '../@types/PropsInterface';
 
 // Need to create TeamContext to provide team list to PlayerList component.
 // This will enable users to select which team to add players to from the PlayerList.
 
-export const TeamContext = () => {
+export const TeamContext = createContext({} as TeamContextType)
+
+export const TeamsProvider = ({ children }: Props) => {
+  const [teams, setTeams] = useState<TeamInterface[] | null>(null);
+
+  useEffect(() => {
+    fetch('teams')
+    .then((res) => {
+      if (res.ok) {
+        res.json()
+        .then(data => setTeams(data))
+      } else {
+        alert('Oops, something went wrong.')
+      }
+    });
+  }, []);
+
+  console.log(teams)
+
   return (
-    <div>TeamContext</div>
+    <TeamContext.Provider value={{ teams, setTeams }}>
+      {children}
+    </TeamContext.Provider>
   )
 }
+
+
+
