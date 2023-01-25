06df83a5a8272405ad0d274bae5830793701a732
Thomas Dimperio <twdimperio@gmail.com>
2023-01-25 10:55:07 -0700

    Build AddPlayer component to allow users to add players to their team(s) via select menu

--- src/components/AddPlayer.tsx
+++ src/components/AddPlayer.tsx
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { useState, useContext} from 'react';
+import { useParams } from 'react-router-dom';
 import { PopupProps } from '../@types/PropsInterface';
+import { TeamContext } from '../context/TeamContext';
 
-export const AddPlayer = (props: PopupProps) => {
-  return props.trigger ? (
+export const AddPlayer = ({trigger, setTrigger}: PopupProps) => {
+
+  const { teams } = useContext(TeamContext);
+  const { id } = useParams();
+
+  const [selectValue, setSelectValue] = useState(teams ? teams[0].id : undefined);
+
+  const onSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    const addedId = id ? parseInt(id) : null
+    
+    const response = await fetch(`/teams/${selectValue}/add_player`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({addedId})
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+      alert("Player added!")
+    } else {
+      console.log(response)
+    }
+  }
+
+  const handleSelectChange = (e: React.FormEvent<HTMLSelectElement>) => {
+    const target = e.target as HTMLButtonElement;
+    setSelectValue(parseInt(target.value));
+  }
+
+  return trigger ? (
     <div>
       <div>
-        <button>Close Popup</button>
-        
+        <form onSubmit={onSubmit}>
+          <label>Select Team</label>
+          <select value={selectValue} onChange={handleSelectChange}>
+            {teams?.map((team) => (
+              <option key={team.id} value={team.id}>{team.name}</option>
+            ))}
+          </select>
+          <input type="submit" value="Add Player" />
+        </form>
+        <button onClick={() => setTrigger(!trigger)}>Cancel</button>
       </div>
     </div>
   ) : <></>;
