13dfeb720e0d2f42d58b20ee592eb9bc32fae5ee
Thomas Dimperio <twdimperio@gmail.com>
2023-02-06 17:26:08 -0700

    Remove unused imports

--- src/Containers/PlayerList.tsx
+++ src/Containers/PlayerList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { PlayerInterface } from '../@types/PlayerInterface';
 import ErrorPage from '../components/ErrorPage';
