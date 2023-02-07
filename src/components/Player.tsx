c6efe137bc387950ac10b3b2fcf3183cafa6f56d
Thomas Dimperio <twdimperio@gmail.com>
2023-02-07 13:48:37 -0700

    Change h4 elements to p. Add PlayerCard to Player component

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -5,6 +5,7 @@ import { UserContext } from '../context/UserContext';
 import { TeamContext } from '../context/TeamContext';
 import {AddPlayer} from './AddPlayer';
 import ErrorPage from './ErrorPage';
+import PlayerCard from './PlayerCard';
 
 
 const Player = () => {
@@ -43,19 +44,20 @@ const Player = () => {
           { user && !trigger ? <button className='button' onClick={handleTrigger}>Add Player</button> : "" }
           <AddPlayer trigger={trigger} setTrigger={setTrigger} />
         </div>
+        <PlayerCard player={player} />
         {player.position !== 'G' ? 
-        <h2>
+        <p>
           Goals per game: {player.skater_stat?.goals / player.skater_stat?.games} <br />
           Assists per game: {player.skater_stat?.assists / player.skater_stat?.games} <br />
           Points per game: {player.skater_stat?.points / player.skater_stat?.games} <br />
           Shots per game: {player.skater_stat?.shots / player.skater_stat?.games} <br />
-        </h2>
+        </p>
          : 
-        <h2>
+        <p>
           Even strength save %: {player.goalie_stat?.even_strength_save_percentage / 100.0} <br />
           Power play save %: {player.goalie_stat?.power_play_save_percentage / 100.0} <br />
           Short-handed save %: {player.goalie_stat?.short_handed_save_percentage / 100.0} <br />
-        </h2>}
+        </p>}
       </div>  
     )
   } else {
--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -6,22 +6,22 @@ const PlayerCard = ({player}: PlayerPropsInterface) => {
   if (player.position !== 'G' && player.skater_stat) {
     return (
       <div className='skater-card'>
-        <h4><span>{player.full_name}, #{player.jersey_number}, {player.position}</span></h4>
-        <h4><span>G: {player.skater_stat?.goals}, A: {player.skater_stat?.assists} ...</span></h4>
+        <p><span>{player.full_name}, #{player.jersey_number}, {player.position}</span></p>
+        <p><span>G: {player.skater_stat?.goals}, A: {player.skater_stat?.assists} ...</span></p>
       </div>
     )
   } else if (player.position === 'G' && player.goalie_stat) {
     return (
       <div className='goalie-card'>
-        <h4><span>{player.full_name}, #{player.jersey_number}, {player.position}</span></h4>
-        <h4><span>W: {player.goalie_stat?.wins} SV%: {player.goalie_stat?.save_percentage}, GAA: {player.goalie_stat?.goals_against_average}</span></h4>
+        <p><span>{player.full_name}, #{player.jersey_number}, {player.position}</span></p>
+        <p><span>W: {player.goalie_stat?.wins} SV%: {player.goalie_stat?.save_percentage}, GAA: {player.goalie_stat?.goals_against_average}</span></p>
       </div>
     )
   } else {
     return (
       <div className='no-stats'>
-        <h4><span>{player.full_name}, #{player.jersey_number}, {player.position}</span></h4>
-        <h4>Player does not have any stats for this season.</h4>
+        <p><span>{player.full_name}, #{player.jersey_number}, {player.position}</span></p>
+        <p>Player does not have any stats for this season.</p>
       </div>
     )
   }
