1f7687af9660e10cddf078fac2a92f4810360037
Thomas Dimperio <twdimperio@gmail.com>
2023-02-07 14:09:45 -0700

    Remove span elements from PlayerCard.

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -6,21 +6,21 @@ const PlayerCard = ({player}: PlayerPropsInterface) => {
   if (player.position !== 'G' && player.skater_stat) {
     return (
       <div className='skater-card'>
-        <p><span>{player.full_name}, #{player.jersey_number}, {player.position}</span></p>
-        <p><span>G: {player.skater_stat?.goals}, A: {player.skater_stat?.assists} ...</span></p>
+        <p>{player.full_name}, #{player.jersey_number}, {player.position}</p>
+        <p>G: {player.skater_stat?.goals}, A: {player.skater_stat?.assists} ...</p>
       </div>
     )
   } else if (player.position === 'G' && player.goalie_stat) {
     return (
       <div className='goalie-card'>
-        <p><span>{player.full_name}, #{player.jersey_number}, {player.position}</span></p>
-        <p><span>W: {player.goalie_stat?.wins} SV%: {player.goalie_stat?.save_percentage}, GAA: {player.goalie_stat?.goals_against_average}</span></p>
+        <p>{player.full_name}, #{player.jersey_number}, {player.position}</p>
+        <p>W: {player.goalie_stat?.wins} SV%: {player.goalie_stat?.save_percentage}, GAA: {player.goalie_stat?.goals_against_average}</p>
       </div>
     )
   } else {
     return (
       <div className='no-stats'>
-        <p><span>{player.full_name}, #{player.jersey_number}, {player.position}</span></p>
+        <p>{player.full_name}, #{player.jersey_number}, {player.position}</p>
         <p>Player does not have any stats for this season.</p>
       </div>
     )
