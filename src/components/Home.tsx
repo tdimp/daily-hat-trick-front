b4440fd9dc2c6eff2b4b868129cba21719aef0a2
Thomas Dimperio <twdimperio@gmail.com>
2023-02-09 17:21:07 -0700

    Build out Home page with current day's NHL games and team cards.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { TeamInterface } from '../@types/TeamInterface';
+import NhlTeamCard from './NhlTeamCard';
 
 const Home = () => {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    fetch('https://statsapi.web.nhl.com/api/v1/schedule')
+    .then(res => res.json())
+    .then(data => setGames(data.dates[0].games))
+  }, [])
+
+  
+
   return (
-    <div>
-      <h1 className='home'>HOME PLACEHOLDER</h1>
+    <div className='container'>
+      {games.map((game: any) => {
+        const awayTeam = game.teams.away.team
+        const homeTeam = game.teams.home.team
+
+        return (
+          <div key={game.id}>
+            <NhlTeamCard key={awayTeam.name} team={awayTeam} />            
+            <h1>@</h1>
+            <NhlTeamCard key={homeTeam.name} team={homeTeam} />
+          </div>
+        )
+      })}
     </div>
   )
 }
