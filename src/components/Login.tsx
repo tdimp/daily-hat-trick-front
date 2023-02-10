5d30e95ecdcb4f719a328bb38be47d55d93560c3
Thomas Dimperio <twdimperio@gmail.com>
2023-02-10 10:18:33 -0700

    Import Bootsrap

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -55,7 +55,7 @@ const Login = () => {
           <input type="submit" value="Login!" />
         </form>
         <>Don't have an account?</>
-        <button className='button' onClick={handleHasAccountClick}>Sign Up</button>
+        <button className="btn btn-primary" onClick={handleHasAccountClick}>Sign Up</button>
       </div>
     )
   } else if(!user && !hasAccount) {
