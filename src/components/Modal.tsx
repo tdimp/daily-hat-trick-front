5e202f62e00fd7f32fa615a164720e04d489869c
Thomas Dimperio <twdimperio@gmail.com>
2023-02-14 12:28:38 -0700

    Update Modal to have correct text

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -11,11 +11,11 @@ const Modal = ({ handleDrop, id }: modalProps) => {
         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div className="modal-body">
-        ...
+        Drop Player?
       </div>
       <div className="modal-footer">
         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary" onClick={() => handleDrop(id)} >Save changes</button>
+        <button type="button" className="btn btn-danger" onClick={() => handleDrop(id)} >Drop</button>
       </div>
     </div>
   </div>
