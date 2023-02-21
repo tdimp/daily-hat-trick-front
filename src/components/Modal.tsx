06bede2abba463c1017f8fd7a67b79970b7aa628
Thomas Dimperio <twdimperio@gmail.com>
2023-02-20 18:35:09 -0700

    Fix player drop modal so that modal is dismissed upon 'Drop' click

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -15,7 +15,7 @@ const Modal = ({ handleDrop, id }: modalProps) => {
       </div>
       <div className="modal-footer">
         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-danger" onClick={() => handleDrop(id)} >Drop</button>
+        <button type="button" className="btn btn-danger" data-bs-dismiss="modal" onClick={() => handleDrop(id)}>Drop</button>
       </div>
     </div>
   </div>
