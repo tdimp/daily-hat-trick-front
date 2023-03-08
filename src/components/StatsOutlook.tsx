a062378b5f0d781990ff9ed4c380bdd078fc0047
Thomas Dimperio <twdimperio@gmail.com>
2023-03-07 17:02:02 -0700

    Refactor fetch URL based on current day of week

--- src/components/StatsOutlook.tsx
+++ src/components/StatsOutlook.tsx
@@ -4,29 +4,30 @@ import { PlayerPropsInterface } from '../@types/PropsInterface';
 // NHL Team endpoint: `https://statsapi.web.nhl.com/api/v1/schedule?teamId=${player.nhl_team.id}&startDate=${startDate}&endDate=${endDate}`
 
 const StatsOutlook = ({ player }: PlayerPropsInterface) => {
-  const [remainingGames, setRemainingGames] = useState(0);
+  const [remainingGames, setRemainingGames] = useState("");
+
   const currentDate = new Date();
   const currentDay = currentDate.getDay();
+  let url: URL;
   let startDate: String;
   let endDate: String;
   let offsetDate;
 
   useEffect(() => {
     if (player.nhl_team) {
-      fetch(`https://statsapi.web.nhl.com/api/v1/schedule?teamId=${player.nhl_team.id}&startDate=${startDate}&endDate=${endDate}`)
+      fetch(url)
       .then(res => res.json())
       .then(data => setRemainingGames(data.totalGames))
     }
   });
 
     if (currentDay === 0) {
-      offsetDate = new Date(new Date().setDate(new Date().getDate() - 6))
-      startDate = `${offsetDate.getFullYear()}-${('0' + (offsetDate.getMonth() + 1)).toString().slice(-2)}-${'0' + offsetDate.getDate().toString().slice(-2)}`
-      endDate = `${currentDate.getFullYear()}-${('0' + (currentDate.getMonth() + 1)).toString().slice(-2)}-${'0' + currentDate.getDate().toString().slice(-2)}`
+      url = new URL(`https://statsapi.web.nhl.com/api/v1/schedule?teamId=${player.nhl_team?.id}`)
     } else if (currentDay === 1) {
       offsetDate = new Date(new Date().setDate(new Date().getDate() + 6))
       startDate = `${currentDate.getFullYear()}-${('0' + (currentDate.getMonth() + 1)).toString().slice(-2)}-${'0' + currentDate.getDate().toString().slice(-2)}`
       endDate = `${offsetDate.getFullYear()}-${('0' + (offsetDate.getMonth() + 1)).toString().slice(-2)}-${'0' + offsetDate.getDate().toString().slice(-2)}`
+      url = new URL(`https://statsapi.web.nhl.com/api/v1/schedule?teamId=${player.nhl_team?.id}&startDate=${startDate}&endDate=${endDate}`)
     } else {
      for (let i = 0; i < 6; i++) {
       if (currentDay - i === 1) {
@@ -34,6 +35,7 @@ const StatsOutlook = ({ player }: PlayerPropsInterface) => {
         let newDate = new Date(new Date().setDate(offsetDate.getDate() + 6))
         startDate = `${offsetDate.getFullYear()}-${('0' + (offsetDate.getMonth() + 1)).toString().slice(-2)}-${'0' + offsetDate.getDate().toString().slice(-2)}`
         endDate = `${newDate.getFullYear()}-${('0' + (newDate.getMonth() + 1)).toString().slice(-2)}-${newDate.getDate().toString().slice(-2)}`
+        url = new URL(`https://statsapi.web.nhl.com/api/v1/schedule?teamId=${player.nhl_team?.id}&startDate=${startDate}&endDate=${endDate}`)
       }
      }  
     }
