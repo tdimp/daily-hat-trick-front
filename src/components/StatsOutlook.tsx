16f3b5afb9c253a2860386e63fd2f7728addef45
Thomas Dimperio <twdimperio@gmail.com>
2023-03-14 18:26:24 -0600

    Change iterable in the else block for loop to start at 1 instead of 2

--- src/components/StatsOutlook.tsx
+++ src/components/StatsOutlook.tsx
@@ -33,7 +33,7 @@ const StatsOutlook = ({ nhlTeamId }: any) => {
       endDate = `${offsetDate.getFullYear()}-${('0' + (offsetDate.getMonth() + 1)).toString().slice(-2)}-${('0' + offsetDate.getDate()).toString().slice(-2)}`
       url = new URL(`https://statsapi.web.nhl.com/api/v1/schedule?teamId=${nhlTeamId}&startDate=${startDate}&endDate=${endDate}`)
     } else {
-     for (let i = 2; i <= 6; i++) {
+     for (let i = 1; i <= 6; i++) {
       if (currentDay - i === 1) {
         offsetDate = new Date(new Date().setDate(new Date().getDate() - i))
         let newDate = new Date(new Date().setDate(offsetDate.getDate() + 6))
@@ -41,7 +41,7 @@ const StatsOutlook = ({ nhlTeamId }: any) => {
         endDate = `${newDate.getFullYear()}-${('0' + (newDate.getMonth() + 1)).toString().slice(-2)}-${('0' + newDate.getDate()).toString().slice(-2)}`
         url = new URL(`https://statsapi.web.nhl.com/api/v1/schedule?teamId=${nhlTeamId}&startDate=${startDate}&endDate=${endDate}`)
       }
-     }  
+     }
     }
 
   return (
