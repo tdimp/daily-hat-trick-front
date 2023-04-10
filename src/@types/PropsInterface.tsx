2a7cbbdd16a253b1206587976f3f94db698dd8d8
Thomas Dimperio <twdimperio@gmail.com>
2023-04-10 14:26:15 -0600

    Remove unnecessary interface declarations

--- src/@types/PropsInterface.tsx
+++ src/@types/PropsInterface.tsx
@@ -1,14 +1,3 @@
-import { NhlTeamInterface } from "./NhlTeamInterface";
-import { PlayerInterface } from "./PlayerInterface";
-
 export interface PropsInterface {
   children: React.ReactNode;
-}
-
-export interface PlayerPropsInterface {
-  player: PlayerInterface;
-}
-
-export interface NhlTeamCardInterface {
-  team: NhlTeamInterface;
 }
