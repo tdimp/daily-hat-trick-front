0e041e2d6e9c9e5ac14720f73adb31152bb262ac
Thomas Dimperio <twdimperio@gmail.com>
2023-04-10 14:25:52 -0600

    Move UserInterface and UserContextType into UserContext, delete unnecessary file

--- src/@types/UserInterface.tsx
+++
@@ -1,9 +0,0 @@
-export interface UserContextInterface {
-  username: string | null;
-  userId: number;
-}
-
-export type UserContextType = {
-  user: UserContextInterface | null;
-  setUser: React.Dispatch<React.SetStateAction<UserContextInterface | null>>
-}
